78fef4af21b351e904c65cd8f6d5ad1bae3f5db6
Fabian Stemmler <stemmler@in.tum.de>
2024-05-01 10:42:53 +0200

    improve widen gas tests
     * rename side -> notify_side in WPointSelect
     * rename trace point "gas" to "widengas" to avoid confusion with context gas
     * fix: widen veto should prevent decrementing gas
     * remove tests that require imprecision
     * remove cram test
     * add new tests

--- tests/regression/82-widening_gas/01-side_parallel.c
+++ tests/regression/82-widening_gas/01-side_parallel.c
@@ -10,36 +10,21 @@ pthread_mutex_t A = PTHREAD_MUTEX_INITIALIZER;
 
 void *increase_to_3(void *arg) {
   for(int i = 0; i < 3; i++) {
-    if(i < 3) {
-      pthread_mutex_lock(&A);
-      a = i;
-      b = i;
-      c = i;
-      pthread_mutex_unlock(&A);
-    }
+    pthread_mutex_lock(&A);
+    a = i;
+    b = i;
+    c = i;
+    pthread_mutex_unlock(&A);
   }
   return NULL;
 }
 
 void *increase_to_4(void *arg) {
   for(int i = 0; i < 4; i++) {
-    if(i < 4) {
-      pthread_mutex_lock(&A);
-      b = i;
-      c = i;
-      pthread_mutex_unlock(&A);
-    }
-  }
-  return NULL;
-}
-
-void *increase_to_5(void *arg) {
-  for(int i = 0; i < 5; i++) {
-    if(i < 5) {
-      pthread_mutex_lock(&A);
-      c = i;
-      pthread_mutex_unlock(&A);
-    }
+    pthread_mutex_lock(&A);
+    b = i;
+    c = i;
+    pthread_mutex_unlock(&A);
   }
   return NULL;
 }
@@ -49,7 +34,6 @@ int main(void) {
   pthread_t id;
   pthread_create(&id, NULL, increase_to_3, NULL);
   pthread_create(&id, NULL, increase_to_4, NULL);
-  pthread_create(&id, NULL, increase_to_5, NULL);
 
   pthread_mutex_lock(&A);
   __goblint_check(a >= 0);
@@ -57,9 +41,6 @@ int main(void) {
 
   __goblint_check(b >= 0);
   __goblint_check(b <= 4);
-
-  __goblint_check(c >= 0);
-  __goblint_check(c <= 5); // UNKNOWN (widen)
   pthread_mutex_unlock(&A);
 
   return 0;
